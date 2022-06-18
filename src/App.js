7f19ff43c7acc3b6509fe2589db4bfc8f8b76b79
Zarni <szarnit94@gmail.com>
2022-06-18 17:58:35 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dude, It's always Hello Wolrd. Why not Hi There? 
         </p>
         <a
           className="App-link"
